1f3932800def063d5a067466fe4597ad272f931d
Achira Wijesuriya <wijesuriya4me@gmail.com>
2025-08-01 22:39:23 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prescription System</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Welcome to Prescription System</h2>
+        <p><a href="user/login.php">User Login</a></p>
+        <p><a href="pharmacy/login.php">Pharmacy Login</a></p>
+    </div>
+</body>
+</html>
